02e2890c869478f6b0d3fb6b67e275adec789b49
Olivier BONNAURE <olivier@solisoft.net>
2021-11-26 17:13:12 +0100

    debug

--- src/editor.js
+++ src/editor.js
@@ -363,6 +363,7 @@
     */
     var add_drop_empty = function () {
       clear_empty_drags()
+      console.log("add_drop_empty")
       var empty = '<div class="sg-row cms_row" data-type="full"><div class="col-12 cms_col"><div class="drop drop_empty row_empty"></div></div></div>'
       var drop_empty = '<div class="drop drop_empty col_empty"><div></div></div>'
 
